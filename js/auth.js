4edd4434a0fbe32bf275b5aad05527d7ed68a7af
Seith256 <musikaseezi@gmail.com>
2025-06-26 15:42:42 +0300

    Delete js/auth.js

--- js/auth.js
+++
@@ -1,9 +0,0 @@
-// auth.js
-export async function authFetch(url, options = {}) {
-    const token = sessionStorage.getItem("aab_admin_token") || sessionStorage.getItem("aab_user_token");
-    if (!options.headers) options.headers = {};
-    if (token) {
-        options.headers["Authorization"] = `Bearer ${token}`;
-    }
-    return fetch(url, options);
-}
